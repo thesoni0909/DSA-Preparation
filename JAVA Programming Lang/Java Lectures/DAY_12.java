edcd629cbbc0d666fc2c8e2beadd4e7b814d27f8
Soni Kumari <thissoni0909@gmail.com>
2026-03-08 00:44:55 +0530

    DAY_12 changes

--- JAVA Programming Lang/Java Lectures/DAY_12.java
+++ JAVA Programming Lang/Java Lectures/DAY_12.java
@@ -167,31 +167,31 @@ public class DAY_12 {
         Scanner sc=new Scanner(System.in);
 
         // creation
-        // int matrix[][]=new int [3][3];
-        // int n=matrix.length;
-        // int m=matrix[0].length;
+        int matrix[][]=new int [3][3];
+        int n=matrix.length;
+        int m=matrix[0].length;
 
         // Input values of 2D Array
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         matrix[i][j]=sc.nextInt();
-        //     }
-        // }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }
 
         // Printing values of 2D Array
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         System.out.print(matrix[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
 
-        // SearchElement(matrix,7);
+        SearchElement(matrix,7);
 
-        // int Arr2D[][]={{1,3,2,4},{9,10,12,11},{6,5,8,7}};
-        // LargestElement(Arr2D);
-        // SmallestElement(Arr2D);
-        // FirstRow(Arr2D);
+        int Arr2D[][]={{1,3,2,4},{9,10,12,11},{6,5,8,7}};
+        LargestElement(Arr2D);
+        SmallestElement(Arr2D);
+        FirstRow(Arr2D);
 
         int SpiralMatrix[][]={{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}};
         SpiralMatrix(SpiralMatrix);
@@ -201,6 +201,8 @@ public class DAY_12 {
         int SortedMatrix[][]={{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
         StaircaseSearch(SortedMatrix,33);
         StaircaseSearch01(SortedMatrix, 45);
+
+        sc.close();
 
     }
 }
