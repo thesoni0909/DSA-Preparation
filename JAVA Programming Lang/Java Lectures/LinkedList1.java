06c1ecc175f623b6e478cf9bfa51aa765b96d413
Soni Kumari <thissoni0909@gmail.com>
2026-06-20 19:31:26 +0530

    linkedlist fundamentals

--- JAVA Programming Lang/Java Lectures/LinkedList1.java
+++ JAVA Programming Lang/Java Lectures/LinkedList1.java
@@ -12,10 +12,12 @@ public class LinkedList1 {
     }
     public static Node Head;
     public static Node Tail;
+    public static int size;
 
     // function for adding nodes at first position in linked list
     public void addFirst(int data){
         Node newNode = new Node(data);
+        size++;
         if(Head == null){
             Head = newNode;
             Tail = newNode;
@@ -28,6 +30,7 @@ public class LinkedList1 {
     // function for adding nodes at last position in linked list
     public void addLast(int data){
         Node newNode = new Node(data);
+        size++;
         if(Head == null){
             Head = newNode;
             Tail = newNode;
@@ -35,6 +38,22 @@ public class LinkedList1 {
         }
         Tail.next = newNode;
         Tail = newNode;
+    }
+
+    // function for adding nodes in middle of a linkedlist
+    public void addMiddle(int index, int data){
+        if(index==0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = Head;
+        for(int i=0; i!=index-1; i++){
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
     }
 
     // function for printing the nodes of linked list
@@ -53,14 +72,21 @@ public class LinkedList1 {
 
     public static void main(String[] args) {
         LinkedList1 list = new LinkedList1();
-        list.print();
-        list.addFirst(1);
-        list.print();
-        list.addFirst(2);
-        list.print();
+        // list.print();
+        // list.addFirst(1);
+        // list.print();
+        // list.addFirst(2);
+        // list.print();
+        // list.addLast(3);
+        // list.print();
+        // list.addLast(4);
+        // list.print();
+        list.addLast(1);
+        list.addLast(2);
         list.addLast(3);
-        list.print();
         list.addLast(4);
+        list.addMiddle(2, 9);
         list.print();
+        System.out.println("Size of Linked List : "+LinkedList1.size);
     }
 }
